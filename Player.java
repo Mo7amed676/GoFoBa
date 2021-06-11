66c4258412b6aa1042eced525cb294e2c943dfa9
Mo7amed676 <84755046+Mo7amed676@users.noreply.github.com>
2021-06-11 22:55:18 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,6 +7,9 @@
  * 
  * class player  extends from user  has one attribute complaint
  */
+
+import java.util.Scanner;
+
 public class Player extends User {
 
    
@@ -19,12 +22,34 @@ public class Player extends User {
         
     }
 
+   
+    @Override
+    public void createProfile() {
+        register();
+    }
+
+    public void register() {
+        Scanner i = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = i.next();
+        System.out.println("enter phone");
+        String phone = i.next();
+        System.out.println("enter email");
+        String email = i.next();
+        System.out.println("enter id");
+        int id = i.nextInt();
+        System.out.println("enter password");
+        String password = i.next();
+        Profile pr = new Profile(name, phone, email, id, password);
+
+    }
+
 
     /**
      * function to make player join team
      */
     public void joinTeam() {
-        throw new UnsupportedOperationException();
+        
     }
 
 
@@ -32,15 +57,12 @@ public class Player extends User {
      *function to select freeTimeslot
      */
     public void selectFreeTimeSlot() {
-        throw new UnsupportedOperationException();
+        
     }
 
 
     /**
      * function to calculateTotalPrice
      */
-    public void calculateTotalPrice() {
-        throw new UnsupportedOperationException();
-    }
 
 }
