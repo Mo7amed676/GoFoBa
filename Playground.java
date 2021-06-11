580a868bb260c3d7e56f7599fa6c1697cd503e31
MohamedOsama2222 <85037885+MohamedOsama2222@users.noreply.github.com>
2021-06-11 19:27:35 +0200

    Update Playground.java

--- Playground.java
+++ Playground.java
@@ -1,6 +1,13 @@
-
 /**
  *
+ * @author Mohamed
+ * @since  11 june 2021
+ *  @version 1.0
+ */
+
+/**
+ *this class add information about playground(name, location
+ * size,hours)
  * @author Eng.Mohamed
  */
 public class Playground {
@@ -12,6 +19,13 @@ public class Playground {
     int arr[] = new int[5];
     int arr2[] = new int[5];
 
+    /**
+     *Parameterized Constructor that set the playground Data
+     * @param name name of playground 
+     * @param location location of playground
+     * @param size size of playground
+     * @param hours  Reservation hours
+     */
     public Playground(String name, String location, double size, int hours) {
         this.name = name;
         this.location = location;
@@ -22,41 +36,84 @@ public class Playground {
         int arr2[] = new int[5];
     }
 
+    /**
+     * default Constructor
+     */
     public Playground() {
     }
 
+    /**
+     * function to get name of playground
+     * @return the value of name of playground
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * function to set name of playground
+     * @param aName  the name of playground
+     */
     public void setName(String aName) {
         this.name = aName;
     }
 
+    /**
+     *function to get location of playground
+     * @return the value of location  of playground
+     * 
+     */
     public String getLocation() {
         return this.location;
     }
 
+    /**
+     *function to set location of playground
+     * 
+     * @param aLocation the location  of playground
+     */
     public void setLocation(String aLocation) {
         this.location = aLocation;
     }
 
+    /**
+     * function to get size of playground
+     * @return  the value of size playground
+     */
     public double getSize() {
         return this.size;
     }
 
+    /**
+     * function to set size of playground
+     * @param aSize the size of playground
+     */
     public void setSize(double aSize) {
         this.size = aSize;
     }
 
+    /**
+     * function to get hour
+     * @return the value of hour
+     */ 
     public int getHours() {
         return this.hours;
     }
 
+    /**
+     * function to set hour
+     * @param aHours  Reservation hours
+     */
     public void setHours(int aHours) {
         this.hours = aHours;
     }
 
+    /**
+     *
+     * @param from 
+     * @param to
+     * @param count
+     */
     public void storeData(int from, int to, int count) {
         for (int i = 0; i < count; i++) {
             arr[i] = from;
@@ -64,6 +121,9 @@ public class Playground {
         }
     }
 
+    /**
+     *function to display avalible hour in playground
+     */
     public void showData() {
         for (int i = 0; i < 2; i++) {
             System.out.print("from ");
@@ -73,10 +133,16 @@ public class Playground {
         }
     }
 
+    /**
+     *   
+     */
     public void availableHours() {
 
     }
 
+    /**
+     *function to display pricePerHour
+     */
     public void pricePerHour() {
 
     }
