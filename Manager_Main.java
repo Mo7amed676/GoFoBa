071e3a66a927fcf1457a988eac5a652043a66458
MohamedOsama2222 <85037885+MohamedOsama2222@users.noreply.github.com>
2021-06-11 19:26:35 +0200

    Update Manager_Main.java

--- Manager_Main.java
+++ Manager_Main.java
@@ -1,24 +1,23 @@
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Eng.Mohamed
+/****
+ *@author Mohamed
+ * @since  11 june 2021
+ *  @version 1.0
+ * 
  */
 public class Manager_Main {
 
     /**
-     *
+     *** This is the main method.
+    * It is for testing purposes to test 
+    ** This is the main method.
+    * It is for testing purposes to test 
+    * the work of GOFO Football
      * @param args
      */
     public static void main(String[] args){
+        // declre object from user
         User u=new User();
-        //u.createProfile();
+       // declre object from PlaygroundOwner
         PlaygroundOwner po=new PlaygroundOwner();
         po.addAvailableHours();
         
