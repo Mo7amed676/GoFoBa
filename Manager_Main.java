5a58fe5ff9f73d961e667780f674972aec7c291e
Youssef-wael18 <85046161+Youssef-wael18@users.noreply.github.com>
2021-06-12 19:28:32 +0200

    Update Manager_Main.java

--- Manager_Main.java
+++ Manager_Main.java
@@ -6,7 +6,7 @@
  */
 public class Manager_Main { 
 
-   /**
+  /**
 	 *** This is the main method. It is for testing purposes to test This is the main
 	 * method. It is for testing purposes to test the work of GOFO Football
 	 * 
@@ -15,7 +15,9 @@ public class Manager_Main {
 	public static void main(String[] args) {
 		PlaygroundOwner po = new PlaygroundOwner();
 		Playground gr = new Playground();
+		System.out.println("registering playground owner");
 		po.register(po, gr);
+		System.out.println("registering playground");
 		po.addPlayground(gr);
 		po.addAvalibleHours(gr);
 		po.addPriceForHours(gr);
@@ -23,9 +25,14 @@ public class Manager_Main {
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		Player pl = new Player();
+		System.out.println("registering player");
 		pl.register(pl);
+		System.out.println("desplay the play ground  and book a free slot");
+		// we should replace it with array list but we do that just for testing the
+		// functions as demanded
 		pl.displayPlayground(gr);
 		pl.selectFreeTimeSlot(gr);
+		System.out.println("desplay the play ground after booking a free slot");
 		pl.displayPlayground(gr);
 
 	}
